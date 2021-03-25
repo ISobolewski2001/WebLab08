582bd6c33a11b41ee7fdbc6ac3e9bbe49704783e
ISobolewski2001 <igor.sobolewski@microsoft.wsei.edu.pl>
2021-03-25 17:40:41 +0100

    Commit

---
+++ Lab08/AnotherWEBPAGE/Igor.py
@@ -0,0 +1,9 @@
+class IGOR(object):
+	def __init__(self, AGE, HEIGHT):
+		super(ClassName, self).__init__()
+		self.AGE = AGE
+		self.HEIGHT = HEIGHT
+
+
+		
+		
---
+++ Lab08/AnotherWEBPAGE/suma_roznica.py
@@ -0,0 +1,26 @@
+#LISTA KROKÓW ALGORYTMU
+
+Dwie Liczby całkowite x i y
+Wynik:
+Suma > Różnica
+Opis : Jeżeli x zsumowane z y daje większy wynik niż 
+Suma < Różnica to output = "Suma większa od różnicy"
+
+
+#PRZYDZIAŁ_GRUPY
+
+import random
+
+Uczeń = random.randint(0 , 60)
+ 
+if Uczeń < 20:
+	print("Grupa Podstawowa")
+elif Uczeń < 40 and > 20:
+	print("Grupa Średniozaawansowana")
+elif Uczeń < 60 and > 40:
+	print("Grupa Zaawansowana")
+else:
+	print("Żadna Grupa")
+
+
+#LISTA KROKÓW ALGORYTMU
